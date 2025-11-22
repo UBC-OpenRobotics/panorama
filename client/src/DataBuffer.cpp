49407549a1107f28193491098792012ecf1d46ff
Nami Maleki <namimaleki13@gmail.com>
2025-11-22 14:55:20 -0800

    skeleton code added

---
+++ client/src/DataBuffer.cpp
@@ -0,0 +1,54 @@
+#include "DataBuffer.hpp"
+
+DataBuffer::DataBuffer() {
+    // Initialize anything needed (nothing required for now)
+}
+
+DataBuffer::~DataBuffer() {
+    // Cleanup (again, nothing required for std::string)
+}
+
+void DataBuffer::writeData(const std::string& jsonChunk) {
+    // TODO: append jsonChunk to buffer_
+    // If using a mutex, lock before modifying buffer_
+}
+
+void DataBuffer::setData(const std::string& jsonData) {
+    // TODO: replace buffer_ with jsonData
+}
+
+std::string DataBuffer::readAll() const {
+    // TODO: return full contents of buffer_ without clearing it
+}
+
+std::string DataBuffer::consume() {
+    // TODO: return all data AND clear buffer_
+}
+
+std::string DataBuffer::extractNextJson() {
+    // TODO:
+    // 1. Look for a full JSON object using findJsonBoundary()
+    // 2. Extract it from buffer_
+    // 3. Remove it from buffer_
+    // 4. Return it
+}
+
+bool DataBuffer::hasCompleteJson() const {
+    // TODO: returns true if findJsonBoundary() != npos
+}
+
+void DataBuffer::clear() {
+    // TODO: clear buffer_
+}
+
+size_t DataBuffer::size() const {
+    // TODO: return buffer_.size()
+}
+
+size_t DataBuffer::findJsonBoundary() const {
+    // TODO:
+    // 1. Detect end of a complete JSON object
+    //    e.g., brace count returns to zero
+    // 2. Return that index or std::string::npos
+    return 0; // placeholder
+}
