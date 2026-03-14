2d1e1d12c16abf9b927438971c5f9467ddaa42a6
Bianca <ly.biancali@gmail.com>
2026-03-14 13:13:33 -0700

    Added wxMathPlot area

--- client/include/client/graph_panel.hpp
+++ client/include/client/graph_panel.hpp
@@ -1,23 +1,11 @@
-
 #pragma once
 #include <wx/wx.h>
-#include <wx/dcbuffer.h>
-#include <wx/window.h>
-#include <vector>
+#include <mathplot.h>
 
 class GraphPanel : public wxPanel {
 public:
     GraphPanel(wxWindow* parent);
 
 private:
-    // Event handlers
-    void OnPaint(wxPaintEvent& event);
-    void OnSize(wxSizeEvent& event);
-
-    // Drawing functions
-    void DrawBackground(wxDC& dc);
-    void DrawGrid(wxDC& dc);
-    void DrawAxes(wxDC& dc);
-
-    wxDECLARE_EVENT_TABLE();
+    mpWindow* m_plot;
 };
