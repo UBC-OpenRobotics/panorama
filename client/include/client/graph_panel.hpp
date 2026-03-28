0c97d4dd2713843617f1afbc18687e478d345b0e
annhypen <haulia368@gmail.com>
2026-03-28 15:44:48 -0700

    Fixed a deadlock in reset method

--- client/include/client/graph_panel.hpp
+++ client/include/client/graph_panel.hpp
@@ -6,7 +6,6 @@
 #include <set>
 #include <string>
 #include <memory>
-#include <mutex>
 #include "client/post_processing.hpp"
 
 class GraphPanel : public wxPanel {
@@ -35,5 +34,4 @@ private:
     wxDECLARE_EVENT_TABLE();
 
     std::shared_ptr<PostProcessing> postProcessor_;
-    std::mutex dataMutex_;
 };
